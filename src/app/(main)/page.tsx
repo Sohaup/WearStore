1c7c25e405ecb7633a9d0ab84b804b0794fd21b5
Sohaup <sohybta560@gmail.com>
2026-01-14 16:18:10 +0200

    initial commit

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Wear store</h1>
+    </div>
+  );
+}
